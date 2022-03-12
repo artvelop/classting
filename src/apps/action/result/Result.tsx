16d3cdbb82634bac38de90f72084675170d0fc42
ruso99 <hondongk@naver.com>
2022-03-12 16:30:23 +0900

    [ADD] result 화면 UI 구현중

--- src/apps/action/result/Result.tsx
+++ src/apps/action/result/Result.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { PageLayout } from '@components/PageLayout';
+import styled from '@emotion/styled';
+import { Typography } from '@mui/material';
+import { themeColor } from '@constants/themeColor';
 
 export const Result = () => {
   return (
     <PageLayout>
-      <div>Result</div>
+      <Container>
+        <TitleContainer>
+          <Typography variant="h5" color={themeColor.white}>
+            수고하셨습니다 🎉🎉
+          </Typography>
+        </TitleContainer>
+      </Container>
     </PageLayout>
   );
 };
+
+const Container = styled.div`
+  width: 100vw;
+  padding-left: 16px;
+  padding-right: 16px;
+`;
+
+const TitleContainer = styled.div`
+  margin-top: 16px;
+`;
