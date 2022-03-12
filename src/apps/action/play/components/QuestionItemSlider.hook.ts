229869e954c75ade1dd08226918029c984bf7a2a
ruso99 <hondongk@naver.com>
2022-03-12 14:32:08 +0900

    [ADD] 마지막 문제에 result 페이지 이동 기능 추가

--- src/apps/action/play/components/QuestionItemSlider.hook.ts
+++ src/apps/action/play/components/QuestionItemSlider.hook.ts
@@ -1,12 +1,20 @@
 import { useRecoilValue } from 'recoil';
 import { questionList } from '@recoil/atom/questionList';
 import { useKeenSlider } from 'keen-slider/react';
+import { userAnswerList } from '@recoil/atom/userAnswerList';
+import { useNavigate } from 'react-router-dom';
 
 export const useQuestionItemSlider = () => {
   const questionItemList = useRecoilValue(questionList);
+  const userAnswerItemList = useRecoilValue(userAnswerList);
   const [slideRef, instanceRef] = useKeenSlider({ drag: false });
+  const navigate = useNavigate();
 
-  const handleNextStep = () => instanceRef.current?.next();
+  const handleNextStep = () => {
+    userAnswerItemList.length === questionItemList.length
+      ? navigate('/action/result')
+      : instanceRef.current?.next();
+  };
 
   return {
     questionItemList,
