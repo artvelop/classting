cdf663567c9b5c84490c8e512f1058ae4322afac
ruso99 <hondongk@naver.com>
2022-03-14 18:11:20 +0900

    [FIX] useStep hooks 메모이제이션 개선

--- src/hooks/useStep.ts
+++ src/hooks/useStep.ts
@@ -1,7 +1,7 @@
 import { questionList } from '@recoil/atom/questionList';
 import { questionStep } from '@recoil/atom/questionStep';
 import { userAnswerList } from '@recoil/atom/userAnswerList';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilState, useRecoilValue } from 'recoil';
 
@@ -11,16 +11,19 @@ export const useStep = () => {
   const userAnswerItemList = useRecoilValue(userAnswerList);
   const navigate = useNavigate();
 
-  const nextStep = () => {
+  const nextStep = useCallback(() => {
     if (questionItemList.length === userAnswerItemList.length) {
       navigate('/action/result');
       setStep(0);
     } else {
       setStep((cur) => cur + 1);
     }
-  };
+  }, [questionItemList, userAnswerItemList, setStep]);
 
-  const prevStep = () => step > 0 && setStep((cur) => cur - 1);
+  const prevStep = useCallback(
+    () => step > 0 && setStep((cur) => cur - 1),
+    [step, setStep],
+  );
 
   const sequence = useMemo(() => step + 1, [step]);
 
