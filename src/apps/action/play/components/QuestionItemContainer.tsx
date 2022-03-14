d1c144537e4ccfa8981afe5a38a7b17170fc2798
ruso99 <hondongk@naver.com>
2022-03-14 18:51:28 +0900

    [FIX] questionItemContainer 테스트케이스 수정

--- src/apps/action/play/components/QuestionItemContainer.tsx
+++ src/apps/action/play/components/QuestionItemContainer.tsx
@@ -6,9 +6,7 @@ import { QuestionItem } from './QuestionItem';
 
 // TestCase
 // 1. pc환경과 모바일환경에서 ui가 자연스럽게 나타나는지 확인
-// 2. 슬라이더가 드래그 했을 때 넘아가지 않는지 확인
-// 3. 마지막 슬라이더에서 result페이지로 올바르게 넘어가는지 확인
-// 4. 렌더링 최적화 이후 deps에 들어갈 값들이 알맞은 값인지 확인
+// 2. 각 스텝마다 올바르게 데이터를 보여주고 있는지 확인
 
 export const QuestionItemContainer = () => {
   const { currentQuestionItem } = useQuestionItemContainer();
